3c24a355214e60c100b1b12b3a96464adb8a4193
mandeepSingh <mandeepraj2312@gmail.com>
2025-06-24 22:13:38 +0530

    feat: add debouncing to search query

--- client/src/components/Search/StudentSearch.tsx
+++ client/src/components/Search/StudentSearch.tsx
@@ -1,7 +1,16 @@
 import type { StudentType } from "@/types/student.types";
+
 import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import studentData from "../../data/students_data.json";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 import { Input } from "../ui/input";
 import { SearchItem } from "./SearchItem";
 export const StudentSearch = () => {
@@ -9,12 +18,19 @@ export const StudentSearch = () => {
   const [data, setData] = useState<StudentType[]>([]);
 
   useEffect(() => {
-    const Data = studentData.filter((student) =>
-      student.name.toLowerCase().startsWith(query)
-    );
+    const timerId = setTimeout(() => {
+      if (query.length >= 3) {
+        const Data = studentData.filter((student) =>
+          student.name.toLowerCase().startsWith(query)
+        );
+        setData(Data);
+      } else {
+        return setData([]);
+      }
+    }, 300);
 
-    setData(Data);
-  }, [query, data]);
+    return () => clearTimeout(timerId);
+  }, [query]);
 
   return (
     <div className="w-full max-w-md">
@@ -35,11 +51,36 @@ export const StudentSearch = () => {
           <div className="py-2">
             {data.length ? (
               data.map((student) => (
-                <SearchItem
-                  key={`${student.rollNumber}`}
-                  name={student.name}
-                  rollNumber={student.rollNumber}
-                />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <button className="w-full text-start">
+                      <SearchItem
+                        key={`${student.rollNumber}`}
+                        name={student.name}
+                        rollNumber={student.rollNumber}
+                      />
+                    </button>
+                  </DialogTrigger>
+                  <DialogContent className="p-2 ">
+                    <DialogHeader>
+                      <DialogTitle className="text-center">
+                        Student Details
+                      </DialogTitle>
+                    </DialogHeader>
+                    <div className="flex justify-center gap-8 ">
+                      <div className="flex flex-col gap-2 ">
+                        <div className="font-medium">Name</div>
+                        <div className="font-medium">Roll No</div>
+                        <div className="font-medium">Class</div>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <div className="">{student.name}</div>
+                        <div className="">{student.rollNumber}</div>
+                        <div className="">{student.class}</div>
+                      </div>
+                    </div>
+                  </DialogContent>
+                </Dialog>
               ))
             ) : (
               <div className="text-sm text-gray-500 dark:text-gray-400 px-4">
