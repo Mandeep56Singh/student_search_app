423196344bdd07480a24204e4ed722bbcaa7e273
mandeepSingh <mandeepraj2312@gmail.com>
2025-06-25 17:46:32 +0530

    perf: add streaming for reading local json improving performance for big json file

--- server/src/service/student.service.ts
+++ server/src/service/student.service.ts
@@ -1,17 +1,48 @@
-import { Request, Response } from "express";
-import data from "../data/student_data.json";
+import fs from "fs";
+import path from "path";
+import { chain } from "stream-chain";
+import streamJson from "stream-json";
+import streamArrayMod from "stream-json/streamers/StreamArray";
 import { StudentType } from "../types/student.type";
 
+const parser = streamJson.parser;
+const streamArray = streamArrayMod.streamArray;
 export const searchStudent = async (
   query: string,
   offset: number
 ): Promise<StudentType[]> => {
-  const q = query.trim().toLowerCase();
-  const students: StudentType[] = data;
+  return new Promise((resolve, reject) => {
+    const matches: StudentType[] = [];
 
-  const filteredStudents = students.filter((student) =>
-    student.name.toLowerCase().startsWith(q)
-  );
+    const filePath = path.resolve(import.meta.dirname, "../data/student_data.json");
 
-  return filteredStudents.slice(0, offset);
+
+    const pipeline = chain([
+      fs.createReadStream(filePath),
+      parser(),
+      streamArray(),
+    ]);
+
+    pipeline.on("data", (data) => {
+      const student: StudentType = data.value;
+
+      if (student.name.toLowerCase().startsWith(query.toLowerCase())) {
+        matches.push(student);
+
+        // Stop when we have enough matches
+        if (matches.length >= offset) {
+          pipeline.destroy();
+          resolve(matches);
+        }
+      }
+    });
+
+    pipeline.on("end", () => {
+      resolve(matches);
+    });
+
+    pipeline.on("error", (err) => {
+      reject(err);
+    });
+  });
 };
